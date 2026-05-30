4332b4a05ca402b245bdf5cf3d0a452574461d55
Vitumbiko-Kamanga <yasharkombi@gmail.com>
2026-05-30 14:08:55 +0200

    Polished solution with Comments added

--- main.go
+++ main.go
@@ -2,32 +2,15 @@ package main
 
 import (
     "fmt"
-    //"math/rand"
-    //"math"
 )
-/*
-// func sum(n []float64) float64{
-// total := 0.0
-// for _,v := range n{
-//     total +=v
-// }
-// return total
-// }
-
-func avg(n []float64) float64{
-    av := 0.0
-    total := sum(n)
-    av = total / 6.0
-    return av
-}
-*/
+// the function for perfoming the number squaring
 func square( n int) int{
     return n*n
     
 }
 
-
-func oldNums(n int) (int){
+// function for generating the sum of Old Perfect Square numbers
+func oldPerfectSquareNums(n int) (int){
     m := []int {}
     sum := 0
     for i := 1; i <= n; i++ {
@@ -41,22 +24,7 @@ func oldNums(n int) (int){
 }
 
 func main() {
-    fmt.Println(oldNums(5))
+    // function calling for finding the sum of Old Perfect Square numbers in a given range (parameter)
+    fmt.Println(oldPerfectSquareNums(5))
 
-
-/*
-    //  fname := "vitumbiko"
-    //  greeting(fname)
-
-    //  //calling the sum function to demonstrate
-    //  grades := []float64 {80, 74, 72, 76, 83, 79} // this is the slice 
-    //  fmt.Printf("the sum of the grades is %v \n",sum(grades))
-    //  fmt.Printf("the average of your grades is %0.2f \n", avg(grades))
-     
-    //  //generation of a random integer between 0-10
-    //  fmt.Println("My random number is ", rand.Intn(10))
-    //  // suing the math library
-    //  fmt.Println("here is the use of a math Library; ", math.Sqrt(16))
-    //  fmt.Printf("here is the use of a math Librar:  %0.3f \n", math.Pi)
-*/
 }
