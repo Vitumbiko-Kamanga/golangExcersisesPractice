f37b1d5718d90ef27210284e0a0c569641ac17a6
Vitumbiko-Kamanga <yasharkombi@gmail.com>
2026-06-02 00:18:57 +0200

    Finished implementation of the fibonacci logic for printing the sum of the fibonacci sum at a specific position (nth position)

--- fibonacciNthNum.go
+++ fibonacciNthNum.go
@@ -1,23 +1,27 @@
 package main
 
-func fibonacci(n int) int{
+import "fmt"
+
+func fibonacci(n int) int {
 	g := 0
-	h := 1
+	h := 0
 	sum := 0
-	for i := 1; i <= n; i++ {
-		if i == 1{
-			sum = g
-			continue
+	for i := 0; i <= n; i++ {
+		if i == 1 {
+			h = 0
+			g = 0
 		}
-		if i == 2 {
-			sum = h
-			continue
+		if i == 1 || i == 2 {
+			h = 1
+			g = 0
 		}
-		
+		sum = g + h
+		g = h
+		h = sum
 	}
 	return sum
 }
 
-func main (){
-
+func main() {
+	fmt.Println(fibonacci(10))
 }
