765ec38ffd3842c4abd621379cc8f1306a4bc80b
Vitumbiko-Kamanga <yasharkombi@gmail.com>
2026-06-02 14:41:26 +0200

    The implementation of a four function calculator with only receiving two arguments

--- commandLineCalaculator.go
+++ commandLineCalaculator.go
@@ -1,35 +1,62 @@
 package main
+
 import (
 	"fmt"
+	"math"
 )
-func add(a int, b int) int{
+func add(a float64, b float64) float64{
 	return a + b
 }
-func subtract (a int, b int) int {
+func divide (a float64, b float64) float64 {
+	if b == 0 {
+		fmt.Println("Err")
+		return math.Inf(1)
+	}
+	return a / b
+}
+func product(a float64, b float64) float64 {
+	return a * b
+}
+func subtract (a float64, b float64) float64 {
 	return a - b
 }
 
-func calculation() {
-	var a, b int
+func calculator() {
+	// variables declaration
+	var a, b float64
 	var symbol string
-	
+
+	// screen formatting 
+	fmt.Println("-------------------------")
+	fmt.Println("WELCOME TO CLI CALCULATOR")
+	fmt.Println("-------------------------")
+
+	// variable entering in the console and reading
 	fmt.Scan( &a, &symbol, &b )
 	fmt.Print("ANS: ")
 
+	// caling different functions for operations
 	if symbol == "+"{
 		fmt.Println(add(a,b))
 	}
 	if symbol == "-"{
 		fmt.Println(subtract(a,b))
 	}
-	if symbol != "+" && symbol != "-" {
+	if symbol == "/"{
+		fmt.Println(divide(a,b))
+	}
+	if symbol == "*"{
+		fmt.Println(product(a,b))
+	}
+	if symbol != "+" && symbol != "-" && symbol != "*"&& symbol != "/"{
 		fmt.Println("Err")
 	}
-}
-func main(){
-	fmt.Println("-------------------------")
-	fmt.Println("WELCOME TO CLI CALCULATOR")
-	fmt.Println("-------------------------")
-	calculation()
+	
+	// end of the calculator console or window.
 	fmt.Println("------THANK YOU----------")
+}
+
+func main(){
+	// function calling for a calculator
+	calculator()
 }
