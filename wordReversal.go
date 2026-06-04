e43ccac1ae93f192d8dfcecd1d731b85da9f88be
Vitumbiko-Kamanga <yasharkombi@gmail.com>
2026-06-04 23:33:20 +0200

    The final implementation of the function to reverse the words which are greater than 5 in length

--- wordReversal.go
+++ wordReversal.go
@@ -4,9 +4,8 @@ import(
 	"strings"
 )
 
-func main (){
-	name := "this is vitumbiko kamanga"
-	name2 := strings.Split(name, " ")
+func toReverse(str string) string{
+	name2 := strings.Split(str, " ")
 	var name3 []string
 	var name4 []string
 	var name5 []string
@@ -19,12 +18,16 @@ func main (){
 			name2[i] = strings.Join(name5, "")
 			name3 = append(name3, " ")
 			name3 = append(name3, name2[i])
+			name5 = nil
 		}else{
 			name3 = append(name3, " ")
 			name3 = append(name3, name2[i])
 		}
 	}
-	name = strings.Join(name3, "")
-	
-	fmt.Println(name)
+	return strings.Join(name3, "")
+}
+
+func main (){
+	name := "this is vitumbiko from blantyre malawi"
+	fmt.Println(toReverse(name))
 }
