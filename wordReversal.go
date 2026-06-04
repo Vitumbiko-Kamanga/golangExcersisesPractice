98b14ece9e616b26ceb92c3ed34d5514ad9c7094
Vitumbiko-Kamanga <yasharkombi@gmail.com>
2026-06-05 00:03:13 +0200

    Changed the spacing formatting in which the space is being added at the end of each word

--- wordReversal.go
+++ wordReversal.go
@@ -16,12 +16,12 @@ func toReverse(str string) string{
 				name5 = append(name5, name4[j])
 			}
 			name2[i] = strings.Join(name5, "")
-			name3 = append(name3, " ")
 			name3 = append(name3, name2[i])
+			name3 = append(name3, " ")
 			name5 = nil
 		}else{
-			name3 = append(name3, " ")
 			name3 = append(name3, name2[i])
+			name3 = append(name3, " ")
 		}
 	}
 	return strings.Join(name3, "")
