84391ead98f080f98ddd7964dd6504b3d489503d
Vitumbiko-Kamanga <yasharkombi@gmail.com>
2026-06-05 20:03:14 +0200

    The finished implementation if the camel case converter function with the one which replaces the underscore, the hiphen and the space and convert the following character to make it camel case which is converted to upper case

--- camelCaseConverter.go
+++ camelCaseConverter.go
@@ -5,15 +5,14 @@ import (
 	"strings"
 )
 
-func main(){
-	name := "vitumbiko-kamanga malawi"
-	name2 := strings.Split(name,"")
+func camelCaseConverter(str string) string{
+	name2 := strings.Split(str,"")
 	var name3 []string
-	for i:= 0; i< len(name); i++{
+	for i:= 0; i< len(str); i++{
 		if name2[i] == "_" || name2[i] == "-" || name2[i] == " "{
 			continue
 		}
-		
+
 		if i >= 1 {
 			j := i-1
 			if name2[j] == "_" || name2[j] == "-" || name2[j] == " "{
@@ -22,5 +21,10 @@ func main(){
 		}
 		name3 = append(name3, name2[i])
 	}
-	fmt.Println(strings.Join(name3,""))
+	return strings.Join(name3,"")
+}
+func main(){
+	name := "vitumbiko-kamanga malawi"
+	fmt.Println(camelCaseConverter(name))
+	
 }
